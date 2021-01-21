49e593e4b1a2680a495088553238df4039c6d4c5
Gaston Rojas <polypuch@gmail.com>
2021-01-21 13:06:33 -0300

    Agregado todos los controladores de Precios

--- controller/c_Precios.js
+++ controller/c_Precios.js
@@ -3,6 +3,11 @@ const precioSchema = require("../models/Precio");
 const asyncHandler = require("../middlewares/async");
 const errorResponse = require("../utils/errorResponse");
 
+/*
+@Description  Elimina una familia
+@Route   DELETE / /apiCatalogo/v0.0.1/:id
+@Access     Private
+*/
 exports.savePrecio = asyncHandler(async (req, res, next) => {
   const precio = await precioSchema.create(req.body);
   res.status(200).json({
@@ -21,5 +26,39 @@ exports.deletePrecio = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: {},
+  });
+});
+
+exports.getPrecios = asyncHandler(async (req, res, next) => {
+  const precio = await precioSchema.find();
+  res.status(200).json({ success: true, data: precio });
+});
+
+exports.getPrecio = asyncHandler(async (req, res, next) => {
+  const precio = precioSchema.findById(req.params.id);
+  if (!precio) {
+    return new errorResponse(
+      `No se encontró lista de precio con el id ${req.params.id}`
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: precio,
+  });
+});
+
+exports.updatePrecio = asyncHandler(async (req, res, next) => {
+  const precio = precioSchema.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!precio) {
+    return new errorResponse(
+      `No se encontró lista de precio con el id ${req.params.id}`
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: precio,
   });
 });
