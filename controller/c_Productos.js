0727fb099d7f7d150dbc391f18a20f888392cd31
Gaston Rojas <polypuch@gmail.com>
2021-01-26 18:03:37 -0300

    Se agrego select fields y sort fields

--- controller/c_Productos.js
+++ controller/c_Productos.js
@@ -41,8 +41,47 @@ exports.deleteProducto = asyncHandler(async (req, res, next) => {
 });
 
 exports.getProductos = asyncHandler(async (req, res, next) => {
-  const producto = await productoSchema.find();
-  res.status(200).json({ success: true, data: producto });
+  let query;
+
+  //Copy req.query
+  const reqQuery = { ...req.query };
+
+  //Fields to exclude
+  const removeFields = ["select", "sort"];
+
+  //Loop over removeFields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]); //Lo remueve para de esta manera no tener problemas con la query en el url
+
+  //convertimos el query a string con json.stringify
+  let queryStr = JSON.stringify(reqQuery);
+  //Reemplazamos lo que empiece con y termine con de las opciones en parentesis
+  queryStr = queryStr.replace(
+    /\b(lt|lte|gte|gt|in)\b/g,
+    (match) => `$${match}` //Match es el valor el cual tomo la regex, al entregarle los backticks con $$match estamos concatenando el $ a la variable match
+  );
+  //finding resource
+  query = productoSchema.find(JSON.parse(queryStr));
+
+  //Select Fields
+  if (req.query.select) {
+    //Separa los campos con un espacio donde encuentre una coma
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields); //le dice a query que los campos seleccionados son los anteriormente manipulados, ya que deben ir con un espacio
+  }
+
+  //Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy); //Ordenara según el orden que le entreguemos los argumentos
+  } else {
+    query = query.sort("nombre"); //Ordenará por defecto con el nombre
+  }
+
+  //executing our query
+  const producto = await query;
+  res
+    .status(200)
+    .json({ success: true, count: producto.length, data: producto });
 });
 
 exports.getProducto = asyncHandler(async (req, res, next) => {
